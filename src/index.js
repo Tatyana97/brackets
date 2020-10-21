0ecf53bc55e0dbda3ee51c5a7b841140389696cd
Tatyana97 <tanyusha.miranovich.97@mail.ru>
2020-10-21 11:26:24 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    var arr = str.split ('');
+
+  for ( var i =0; i<str.length; i++){
+    for( var j = 0; j<bracketsConfig.length; j++){
+      let start = bracketsConfig[j][0];
+      let finish = bracketsConfig[j][1];
+     
+      if(arr[i]==start || arr[i+1] == finish){
+         return true;
+        } else{
+        return  false;
+          }
+      
+      } 
+}
 }
