c455f21867f33209a624b5b05f260e5bdbc81f23
Tatyana97 <tanyusha.miranovich.97@mail.ru>
2021-02-25 22:15:02 +0300

    fix: add change

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 module.exports = function check(str, bracketsConfig) {
-    var arr = str.split ('');
 
-  for ( var i =0; i<str.length; i++){
-    for( var j = 0; j<bracketsConfig.length; j++){
+  if (str.length % 2 == 1 || str.length < 2) {
+    return false;
+}
+
+  let arr = str.split ('');
+//  console.log(arr);
+  for ( let i =0; i<str.length; i++){
+    for( let j = 0; j<bracketsConfig.length; j++){
       let start = bracketsConfig[j][0];
       let finish = bracketsConfig[j][1];
      
@@ -14,4 +19,4 @@ module.exports = function check(str, bracketsConfig) {
       
       } 
 }
-}
+} 
